97cacbe761c5c7ddd7e834619d6fc536c017bc35
ryanl <ryanlimadev@gmail.com>
2022-09-13 11:05:15 -0300

    criando tela de login

--- app/src/main/java/activity/FormLogin.kt
+++ app/src/main/java/activity/FormLogin.kt
@@ -1,12 +1,23 @@
 package activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rlds.lojavirtual.R
+import com.rlds.lojavirtual.databinding.ActivityFormLoginBinding
 
 class FormLogin : AppCompatActivity() {
+    private lateinit var binding:ActivityFormLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_login)
+        binding = ActivityFormLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+        binding.txtTelaCadastro.setOnClickListener {
+            val intent = Intent(this, FormCadastro::class.java)
+            startActivity(intent)
+        }
     }
 }

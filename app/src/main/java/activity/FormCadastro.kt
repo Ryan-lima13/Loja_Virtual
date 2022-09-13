146016550a74f98bcf172a94d7d1e17e0f438274
ryanl <ryanlimadev@gmail.com>
2022-09-13 10:17:07 -0300

    Initial commit

---
+++ app/src/main/java/activity/FormCadastro.kt
@@ -0,0 +1,12 @@
+package activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rlds.lojavirtual.R
+
+class FormCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_cadastro)
+    }
+}

b35097cb4a3951de872570a05ca24d0215358e11
ryanl <ryanlimadev@gmail.com>
2022-09-14 10:43:04 -0300

    deslogando usuario

--- app/src/main/java/activity/TelaProdutos.kt
+++ app/src/main/java/activity/TelaProdutos.kt
@@ -1,12 +1,36 @@
 package activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 import com.rlds.lojavirtual.R
 
 class TelaProdutos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_produtos)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_principal, menu)
+        return  true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.deslogar -> deslogarUsuario()
+
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    private  fun deslogarUsuario(){
+        FirebaseAuth.getInstance().signOut()
+        val intent = Intent(this, FormLogin::class.java)
+        startActivity(intent)
+
     }
 }

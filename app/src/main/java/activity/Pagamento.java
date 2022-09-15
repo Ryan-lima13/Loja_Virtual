fc97d1ebbd65117b3b98800a437346145a097951
ryanl <ryanlimadev@gmail.com>
2022-09-15 12:48:41 -0300

    finalizando projeto

--- app/src/main/java/activity/Pagamento.java
+++ app/src/main/java/activity/Pagamento.java
@@ -2,15 +2,33 @@ package activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.mercadopago.android.px.configuration.AdvancedConfiguration;
+import com.mercadopago.android.px.core.MercadoPagoCheckout;
+import com.mercadopago.android.px.model.Payment;
 import com.rlds.lojavirtual.R;
 import com.rlds.lojavirtual.databinding.ActivityPagamentoBinding;
+
+import interfaceMercadoPago.ComunicacaoServidorMP;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class Pagamento extends AppCompatActivity {
     private ActivityPagamentoBinding binding;
@@ -49,15 +67,114 @@ public class Pagamento extends AppCompatActivity {
 
                 }else {
                     // fazer paagemnto
-                    Snackbar snackbar = Snackbar.make( view,
-                            "fazer paagmento",
-                            Snackbar.LENGTH_SHORT
-                    );
-                    snackbar.setBackgroundTint(Color.RED);
-                    snackbar.setTextColor(Color.WHITE);
-                    snackbar.show();
+                   criarJsonObject();
                 }
             }
         });
     }
+    private void  criarJsonObject(){
+        JsonObject dados = new JsonObject();
+        // priemiro item
+        JsonArray itemLista = new  JsonArray();
+        JsonObject item;
+
+        // segundo item
+        JsonObject email = new JsonObject();
+
+
+
+        item = new JsonObject();
+        item.addProperty("title",nome);
+        item.addProperty("quantity,",1);
+        item.addProperty("currency_id","BRL");
+        item.addProperty("unit_price",Double.parseDouble(preco));
+        itemLista.add(item);
+        dados.add("items", itemLista);
+
+
+        String emailUsuario = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        email.addProperty("email", emailUsuario);
+        dados.add("payer", email);
+        Log.d("j", dados.toString());
+
+        criarPreferenciaPagamento(dados);
+
+
+
+
+    }
+    private  void criarPreferenciaPagamento(JsonObject dados){
+        String site = "https://api.mercadopago.com";
+        String url = "/checkout/preferences?acess_token=" + ACCESS_TOKEN;
+        Gson gson = new GsonBuilder().setLenient().create();
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(site).addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson)).build();
+        ComunicacaoServidorMP conexao_pamento = retrofit.create(ComunicacaoServidorMP.class);
+        Call<JsonObject> request = conexao_pamento.enviarPagamento(url,dados);
+        request.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                String preferenceId = response.body().get("id").getAsString();
+                criarPagamento(preferenceId);
+
+
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+
+            }
+        });
+
+    }
+    private void criarPagamento(String preferenceId){
+        final AdvancedConfiguration advancedConfiguration =
+                new AdvancedConfiguration.Builder().setBankDealsEnabled(false).build();
+        new MercadoPagoCheckout
+                .Builder(PUBLIC_KEY, preferenceId)
+                .setAdvancedConfiguration(advancedConfiguration).build()
+                .startPayment(this, 123);
+
+
+    }
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 123) {
+            if (resultCode == MercadoPagoCheckout.PAYMENT_RESULT_CODE) {
+                final Payment pagamento = (Payment) data.getSerializableExtra(MercadoPagoCheckout.EXTRA_PAYMENT_RESULT);
+                respostaMercadoPago(pagamento);
+
+            } else if (resultCode == RESULT_CANCELED) {
+
+            } else {
+
+            }
+        }
+    }
+    private  void respostaMercadoPago(Payment pagamento){
+        String status = pagamento.getPaymentStatus();
+
+        if(status.equalsIgnoreCase("approved")){
+            Snackbar snackbar = Snackbar.make(binding.container, "Sucesso ao fazer  pagamento!",
+                    Snackbar.LENGTH_SHORT
+
+            );
+            snackbar.setBackgroundTint(Color.BLUE);
+            snackbar.setTextColor(Color.WHITE);
+            snackbar.show();
+
+
+        }else if( status.equalsIgnoreCase("rejected")){
+
+            Snackbar snackbar = Snackbar.make(binding.container, "Erro ao fazer pagamento!",
+                    Snackbar.LENGTH_SHORT
+
+            );
+            snackbar.setBackgroundTint(Color.RED);
+            snackbar.setTextColor(Color.WHITE);
+            snackbar.show();
+        }
+    }
+
 }
